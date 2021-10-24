1c50355be93de02a62eac406e14d1baac87dc0c1
ezegyfa2 <darvasvok@gmail.com>
2021-10-24 03:53:49 +0200

    Fixing folder paths

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 export default {
 	install(Vue, options) {
-		Vue.component('small-text-content', require('SmallTextContent.vue').default);
-		Vue.component('medium-text-content', require('MediumTextContent.vue').default);
-		Vue.component('large-text-content', require('LargeTextContent.vue').default);
-		Vue.component('title-with-icon', require('TitleWithIcon.vue').default);
+		Vue.component('small-text-content', require('./SmallTextContent.vue').default);
+		Vue.component('medium-text-content', require('./MediumTextContent.vue').default);
+		Vue.component('large-text-content', require('./LargeTextContent.vue').default);
+		Vue.component('title-with-icon', require('./TitleWithIcon.vue').default);
 	}
 };
