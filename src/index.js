0a5c4eaac53f67ee7c423f259024ad216eb919ab
ezegyfa2 <darvasvok@gmail.com>
2021-10-25 09:55:13 +0200

    Removing unnecessary parameter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export default {
-	install(Vue, options) {
+	install(Vue) {
 		Vue.component('small-text-content', require('./SmallTextContent.vue').default);
 		Vue.component('medium-text-content', require('./MediumTextContent.vue').default);
 		Vue.component('large-text-content', require('./LargeTextContent.vue').default);
