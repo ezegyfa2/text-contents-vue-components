02166ed80abdf8a2e8693bec2d9ace0166eff7de
ezegyfa2 <darvasvok@gmail.com>
2023-10-05 04:29:27 +0300

    Fixing

--- src/TwoTitleTextContent/Component.vue
+++ src/TwoTitleTextContent/Component.vue
@@ -12,16 +12,6 @@
         props: {
             sub_title: {
                 type: String
-            },
-            added_classes: {
-                type: Object,
-                default () {
-                    return {
-                        '.title': 'h3',
-                        '.sub-title': 'h3',
-                        '.content': 'text-muted'
-                    }
-                }
             }
         }
     }
